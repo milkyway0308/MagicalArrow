16aaf35d850e4200569a1b23bc0834631a54e470
Wat da fak <juni2484ok@naver.com>
2021-03-29 16:27:07 +0900

    Remove unusued publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,29 +58,3 @@ dependencies {
     compileOnly(files("V:/API/Java/Minecraft/Bukkit/Spigot/Spigot 1.12.2.jar"))
 }
 
-
-
-
-publishing {
-    repositories {
-        maven {
-            name = "Github"
-            url = uri("https://maven.pkg.github.com/FUNetwork/MoreShops")
-            credentials {
-                username = properties["gpr.user"] as String
-                password = properties["gpr.key"] as String
-            }
-        }
-    }
-    publications {
-        create<MavenPublication>("jar") {
-            from(components["java"])
-            groupId = "skywolf46"
-            artifactId = "moreshops"
-            version = properties["version"] as String
-            pom {
-                url.set("https://github.com/FUNetwork/MoreShops.git")
-            }
-        }
-    }
-}
